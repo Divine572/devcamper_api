ff61a51f89d2866e83e8a6d28110f8a04050a667
divine favour <divinef837@gmail.com>
2021-08-26 22:06:51 +0100

    added bootcamp model

---
+++ models/Bootcamp.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose');
+
+
+const bootcampSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+        unique: true,
+        maxlength: [50, 'Name should not be above 50 characters'],        
+    },
+    slug: String,
+    description:  {
+        type: String,
+        required: [true, 'Description is required'],
+        maxlength: [500, 'Description should not be above 500 characters'],        
+    },
+    website: {
+        type: String,
+        match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+            'Please use a valid URL with HTTP or HTTPS'
+        ]
+    },
+    phone: {
+        type: String,
+        maxlength: [20, 'Phone number can not be longer than 20 characters']
+    },
+    email: {
+        type: String,
+        match: [
+            /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+            'Please enter a valid email'
+        ]
+    },
+    address: {
+        type: String,
+        required: [true, 'Please enter an address']
+    },
+    location: {
+        // GeoJSON Point
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            index: '2dsphere'
+        },
+        street: String,
+        city: String,
+        state: String,
+        zipCode: String,
+        country: String,
+        formattedAddress: String
+    },
+    careers: {
+        // Array of strings
+        type: [String],
+        required: true,
+        enum: [
+            'Web Development',
+            'Mobile Development',
+            'Data Science',
+            'Business',
+            'UI/UX',
+            'Other'
+        ]
+    },
+    averageRating: {
+        type: Number,
+        min: [1, 'Rating must be at least 1'],
+        max: [10, 'Rating must be more than 10']
+    },
+    averageCost: Number,
+    photo: {
+        type: String,
+        default: 'no-photo.jpg'
+    }, 
+    housing: {
+        type: Boolean,
+        default: false
+    },
+    jobAssistance: {
+        type: Boolean,
+        default: false
+    },
+    jobGuarantee: {
+        type: Boolean,
+        default: false
+    },
+    acceptGi: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+module.exports = mongoose.model('Bootcamp', bootcampSchema);
